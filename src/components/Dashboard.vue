0d4155efe45a5ae883d1b59a9fa434114e315f34
Antonio <antonio.conipo@gmail.com>
2017-06-28 20:07:10 +0200

    Dasboard Component for event managing

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,45 @@
 <template>
-	<div class="t">
-		Dashboard Components
-		{{$store.state}}
+	<div>
+	  <h3>Events Manager</h3>
+	  <button class="btn btn-danger btn-sm signout-btn" @click="signOut">Sign Out</button>
+	  <hr>
+	  <AddEvent />
+	  <hr>
+	  <div class="col-md-12">
+	    <EventItem
+	      v-for="(event_item, index) in this.$store.state.events"
+	      :event="event_item"
+	      key="index"
+	    />
+	  </div>
 	</div>
 </template>
+
+<script>
+import { firebaseApp, eventsRef } from '../firebaseApp'
+import AddEvent from './AddEvent.vue'
+import EventItem from './EventItem.vue'
+
+	export default {
+	  methods: {
+	    signOut() {
+	      this.$store.dispatch('signOut')
+	      firebaseApp.auth().signOut()
+	    }
+	  },
+	  components: {
+	    AddEvent,
+	    EventItem
+	  },
+	  mounted() {
+	  	//read from firebase database and set events to store
+	    eventsRef.on('value', snap => {
+	      let events = []
+	      snap.forEach(event => {
+	        events.push(event.val())
+	      })
+	      this.$store.dispatch('setEvents', events)
+	    })
+	  }
+	}
+</script>
